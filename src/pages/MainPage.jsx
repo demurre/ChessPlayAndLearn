c4f9b4019f76d29bb409fdcec260854c309137dc
demurre <y.drozdov130705@gmail.com>
2023-12-13 00:34:24 +0200

    update images

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const MainPage = () => {
   return (
-    <div className="w-screen h-screen pl-[229px] pt-[57px] bg-zinc-800 justify-start items-center inline-flex">
-      <div className="w-[347px] h-[55px] text-zinc-200 text-[40px] font-normal font-['Satoshi']">
+    <div className="w-screen h-screen bg-zinc-800 justify-center items-center inline-flex">
+      <div className="text-zinc-200 text-[40px] font-normal font-['Satoshi']">
         Welcome back!
       </div>
     </div>
